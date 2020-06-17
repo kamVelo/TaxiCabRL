5edc31e197d2c0ac742029867c6a9863a3c3653f
Ali Kamel <alik87932@gmail.com>
2020-06-17 08:05:42 +0100

    this is a brute force method which doesn't learn it simply chooses random actions and simulates them.

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import gym
 
-env = gym.make("Taxi-v3 ").env
+env = gym.make("Taxi-v3").env
 
-env.render()
+env.s = 328
+
+epochs = 0
+penalties, rewards = 0, 0
+
+frames = []
+done = False
+
+while not done:
+
+    action = env.action_space.sample()
+    state,reward,done,info = env.step(action)
+    if reward == -10:
+        penalties += 1
+
+    frames.append({
+        'frame':env.render(mode="ansi"),
+        'state': state,
+        'action':action,
+        'reward': reward
+    })
+    epochs += 1
+print("Timesteps taken: {}".format(epochs))
+print("Penalties incurred: {}".format(penalties))
